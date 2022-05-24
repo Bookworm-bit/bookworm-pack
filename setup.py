91b5ecd4a49051c4c3f52b54af24cc4c8c6b5ed4
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2022-05-24 12:27:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.readlines()
+
+setuptools.setup(
+    name='bookworm',
+    version='0.0.1',
+    author='Bookworm-bit',
+    author_email='ez2630130@gmail.com',
+    description='Collection of random functions that might be useful',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Bookworm-bit/bookworm-pack',
+    project_urls = {
+        "Issues": "https://github.com/Bookworm-bit/bookworm-pack/issues"
+        "Website": "https://devworm.tk/"
+    },
+    license='MIT',
+    packages=['bookworm'],
+    install_requires=['requests, discord'],
+)
